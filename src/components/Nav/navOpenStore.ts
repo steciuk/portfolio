1559b0562eaba67638c942d961cbefc79af80138
steciuk <a.t.steciuk@gmail.com>
2024-07-31 12:26:07 +0200

    Close nav on Escape press

---
+++ src/components/Nav/navOpenStore.ts
@@ -0,0 +1,3 @@
+import { atom } from "nanostores";
+
+export const navOpen = atom(false);
--- src/components/Timeline/EntryDeselector.svelte
+++ src/components/Timeline/EntryDeselector.svelte
@@ -1,8 +1,13 @@
 <script lang="ts">
+  import { navOpen } from "@components/Nav/navOpenStore";
   import { selectedEntry } from "@components/Timeline/selectedEntryStore";
 
   function handleKeyDown(event: KeyboardEvent) {
-    if ($selectedEntry !== null && event.key === "Escape") {
+    if (
+      $selectedEntry !== null &&
+      event.key === "Escape" &&
+      (!navOpen.get() || window.innerWidth > 1024) // TODO: Change the width if main breakpoint changes
+    ) {
       event.preventDefault();
       selectedEntry.clear();
     }
