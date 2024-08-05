ea6c5df323db2a26c75bed37a913acb825dcb121
steciuk <steciuk77@gmail.com>
2024-08-05 13:27:33 +0000

    Deploying to gh-pages from @ steciuk/portfolio@2b8d8646b583f7022dc52541fbacc10256bfdf26 🚀

---
+++ _astro/entryId.-GtU6zDy.js
@@ -0,0 +1,1 @@
+function e(t){return`${t.collection}_${t.id}`}function n(t){return`entry-details-${e(t)}`}export{n as a,e as g};
---
+++ _astro/selectedEntryStore.CW8nytoz.js
@@ -0,0 +1,1 @@
+import{a as t}from"./index.Mc1atRao.js";function r(){const e=t(null);return{...e,clear:()=>e.set(null)}}const c=r();export{c as s};
