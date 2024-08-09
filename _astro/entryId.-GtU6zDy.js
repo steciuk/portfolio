bbad101e161c9795e1bca42ac8df637de3928bc9
steciuk <steciuk77@gmail.com>
2024-08-09 11:24:27 +0000

    Deploying to gh-pages from @ steciuk/portfolio@c0a44963540ec792939be731a12464865a25d1ca 🚀

--- _astro/entryId.-GtU6zDy.js
+++
@@ -1,1 +0,0 @@
-function e(t){return`${t.collection}_${t.id}`}function n(t){return`entry-details-${e(t)}`}export{n as a,e as g};
---
+++ _astro/entryId.B7QObIhz.js
@@ -0,0 +1,1 @@
+function e(t){return`${t.collection}_${t.id}`}function n(t){return`entry-details-${e(t)}`}export{e as a,n as g};
