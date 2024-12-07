a3b8cf59acbe5617a2cce1852cb1e95dd0c94c56
steciuk <a.t.steciuk@gmail.com>
2024-12-07 13:33:56 +0100

    Add robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,15 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import svelte from "@astrojs/svelte";
-
 import mdx from "@astrojs/mdx";
+
+import robots from "astro-robots";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind({
     applyBaseStyles: false,
     nesting: true
-  }), sitemap(), svelte(), mdx()],
+  }), sitemap(), svelte(), mdx(), robots()],
   site: "https://steciuk.dev"
 });
